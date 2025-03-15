383e764b09cbdc92551055f72a5333310dad80d7
Miguel Rodriguez <miguel.rodriguez@deus.ai>
2025-03-15 20:41:33 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -16,9 +16,55 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConn(conn)
 	}
+}
+
+type HTTPResponse struct {
+	Status       string
+	StatusReason string
+	Headers      map[string]string
+	Body         []byte
+}
+
+func (r HTTPResponse) Write(conn net.Conn) error {
+	// Write the status line
+	_, err := conn.Write(fmt.Appendf(nil, "HTTP/1.1 %s %s\r\n", r.Status, r.StatusReason))
+	if err != nil {
+		return err
+	}
+
+	// Write the headers
+	for key, value := range r.Headers {
+		_, err := conn.Write(fmt.Appendf(nil, "%s: %s\r\n", key, value))
+		if err != nil {
+			return err
+		}
+	}
+	// Mark end of headers
+	conn.Write([]byte("\r\n"))
+
+	// Write the body
+	_, err = conn.Write(r.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	resp := HTTPResponse{
+		Status:       "200",
+		StatusReason: "OK",
+	}
+	resp.Write(conn)
 }
